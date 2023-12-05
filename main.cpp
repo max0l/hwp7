d6ca69028581fcea26edc85092cb8958d230f51b
max0l <mail@maxloebel.de>
2023-12-05 14:16:48 +0100

    Trying to implement Manchester in a hacky way, have'nt tested it yet!

--- main.cpp
+++ main.cpp
@@ -30,3 +30,54 @@ int main() {
     
 
 }
+
+
+void sendDataWithManchester(uint8_t &data, B15F& drv) {
+    drv.setRegister(&PORTA, (0x0F) & data);
+    //drv.delay_us(10);
+    drv.delay_ms(100);
+    drv.setRegister(&PORTA, (data>>4));
+}
+
+
+//This will get only 4 bit at a time
+uint8_t readData(B15F& drv) {
+    uint8_t data;
+    uint8_t firstData = drv.getRegister(&PINA);
+    uint8_t secondData;
+    bool hasChanged = false;
+    while(!hasChanged) {
+        //Doing this in manchester, means that if I reveived the firstData correctly, the secondData has to be the negated version of the first one
+        secondData = drv.getRegister(&PINA);
+        if(firstData != secondData) {
+            if(firstData == ~(0xF0 & secondData)) {
+                hasChanged = true;
+            }
+        }
+    }
+
+    return interpretData(firstData, secondData);
+
+}
+
+uint8_t interpretData(uint8_t &firstData, uint8_t &secondData) {
+    uint8_t solution;
+    for(uint8_t i = 0; i<4; i++) {
+        if((firstData >> i) & 0x01 == 1 && (secondData >> i) & 0x01 == 0) {
+            solution |= (1 << i);
+        }
+    }
+    return solution;
+}
+
+//Instead of sending each bit alone: send 2 4Bit that are together encoded in Manchester
+//For example:
+/*
+0101
+=
+(1.)0101
+(2.)1010
+
+It will read for    PINA0: 10 -> 1
+                    PINA1: 01 -> 0
+*/
